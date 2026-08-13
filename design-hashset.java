ebc30b00ed466af3284bf04644cd3747b26c5661
Pranjal Chavda <pranjalchavda16@gmail.com>
2026-08-13 21:38:49 +0530

    Add design-hashset.java

---
+++ design-hashset.java
@@ -0,0 +1,36 @@
+class MyHashSet {
+    int size=769;
+    LinkedList<Integer>[] buckets;
+
+    public MyHashSet() {
+        buckets=new LinkedList[size];
+    }
+
+    private int hash(int key) {
+        return key%size;
+    }
+
+    public void add(int key) {
+        int index=hash(key);
+        if (buckets[index]==null) buckets[index]=new LinkedList<>();
+        if (!buckets[index].contains(key)) buckets[index].add(key);
+    }
+
+    public void remove(int key) {
+        int index=hash(key);
+        if (buckets[index]!=null) buckets[index].remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        int index=hash(key);
+        return buckets[index]!=null && buckets[index].contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
