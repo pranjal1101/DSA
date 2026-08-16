5f3523503aeb4d48e95d089ca713b74171314eab
Pranjal Chavda <pranjalchavda16@gmail.com>
2026-08-16 22:41:17 +0530

    Add find-n-unique-integers-sum-up-to-zero.java

---
+++ find-n-unique-integers-sum-up-to-zero.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int[] a=new int[n];
+        for(int i=0;i<n/2;i++){
+            a[i]=i+1;
+            a[n-1-i]=-(i+1);
+        }
+        return a;
+    }
+}
