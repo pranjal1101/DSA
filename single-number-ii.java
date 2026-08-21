55771bc70df99403730c6c24ba81bd3b73662db0
Pranjal Chavda <pranjalchavda16@gmail.com>
2026-08-21 17:41:17 +0530

    Add single-number-ii.java

---
+++ single-number-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int x:nums){
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+        for(int x:nums){
+            if(map.get(x)==1){
+                return x;
+            }
+        }
+        return 0;
+    }
+}
