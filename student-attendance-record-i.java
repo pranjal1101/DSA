05f61fcf15285d3663df049c0f53b2b5705e61bb
Pranjal Chavda <pranjalchavda16@gmail.com>
2026-08-27 00:29:37 +0530

    Add student-attendance-record-i.java

---
+++ student-attendance-record-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int a=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='A'){
+                a++;
+            }
+            if(a>1){
+                return false;
+            }
+            if(i>=2&&s.charAt(i)=='L'&&s.charAt(i-1)=='L'&&s.charAt(i-2)=='L'){
+                return false;
+            }
+        }
+        return true;
+    }
+}
